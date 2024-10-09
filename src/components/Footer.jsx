d34876767994fba98a082143fda93765c0ead5e3
shcreator <mainid24@outlook.com>
2024-10-09 10:27:28 +0600

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+// src/components/Footer.jsx
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-gray-100 py-6">
+      <div className="container mx-auto px-4 text-center">
+        <p className="text-gray-600">© 2023 Template Store. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
